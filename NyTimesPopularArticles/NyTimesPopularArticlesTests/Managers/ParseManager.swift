2a7da8cc92af9f584a3a4bb9326262d4b85d412e
Nour2017 <ahmednour.mohamed2012@gmail.com>
2020-06-18 22:44:02 +0200

    Implement nytimes target unit test

---
+++ NyTimesPopularArticles/NyTimesPopularArticlesTests/Managers/ParseManager.swift
@@ -0,0 +1,26 @@
+//
+//  ParseManager.swift
+//  NyTimesPopularArticlesTests
+//
+//  Created by Ahmed Nour on 6/18/20.
+//  Copyright © 2020 Ahmed Nour. All rights reserved.
+//
+
+import Foundation
+class ParseManager<T: Codable> {
+    func parseAndLoadJson(filename fileName: String) -> T? {
+      if let url = Bundle(for: ParseManager.self).url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let dataDecoded = try decoder.decode(T.self, from: data)
+                return dataDecoded
+
+            } catch {
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+}
---
+++ NyTimesPopularArticles/NyTimesPopularArticlesTests/Views/DetailsViewTests.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsViewTests.swift
+//  NyTimesPopularArticlesTests
+//
+//  Created by Ahmed Nour on 6/18/20.
+//  Copyright © 2020 Ahmed Nour. All rights reserved.
+//
+
+import XCTest
+@testable import NyTimesPopularArticles
+class DetailsViewTests: XCTestCase {
+  var detailsViewController : NewsDetailsViewController?
+    override func setUp() {
+      let results = ParseManager<Results>().parseAndLoadJson(filename:"Results")
+      detailsViewController = NewsDetailsViewController(data: results!)
+    }
+
+   func test_ViewController_WhenIntialized_NotNull() {
+       XCTAssertNotNil(detailsViewController, "null error")
+   }
+  func test_viewDidLoad_always_intializeData() {
+      let viewController = detailsViewController!
+      _ = viewController.view
+      viewController.viewDidLoad()
+      XCTAssertNotNil(detailsViewController?.view, "null error")
+  }
+}
